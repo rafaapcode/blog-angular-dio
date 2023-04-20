ba596c190209292eb1fab2f53ff2ad51e3ef5dcc
rafaapcode <rafaap2003@gmail.com>
2023-04-20 07:37:33 -0300

    feat: creating a logic component

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent {
+
+  @Input() photoCover: string = '';
+  title: string = 'Teste de título';
+  description: string = 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page'
 
 }
